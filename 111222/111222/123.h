04bf212824876eedbb4f3778c9d713f388b5d706
lanou <lan@Chi66deMac-mini.local>
2016-09-05 20:34:11 +0800

    啊啊啊

---
+++ 111222/111222/123.h
@@ -0,0 +1,13 @@
+//
+//  123.h
+//  111222
+//
+//  Created by lanou on 16/9/5.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface _23 : NSObject
+
+@end
---
+++ 111222/111222/123.m
@@ -0,0 +1,13 @@
+//
+//  123.m
+//  111222
+//
+//  Created by lanou on 16/9/5.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import "123.h"
+
+@implementation _23
+
+@end
