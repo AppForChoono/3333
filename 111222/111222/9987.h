b69988d9ba45b2bf5afb1dbc7045d3004b721061
lanou <lan@xiaoqideMac-mini.local>
2016-09-05 20:39:35 +0800

    2323232

---
+++ 111222/111222/9987.h
@@ -0,0 +1,13 @@
+//
+//  9987.h
+//  111222
+//
+//  Created by lanou on 16/9/5.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface _987 : NSObject
+
+@end
---
+++ 111222/111222/9987.m
@@ -0,0 +1,13 @@
+//
+//  9987.m
+//  111222
+//
+//  Created by lanou on 16/9/5.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import "9987.h"
+
+@implementation _987
+
+@end
